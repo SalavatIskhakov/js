85d0798649a1f6bf04a0d4d52f2666044438f419
NatGames228 <61728582+NatGames228@users.noreply.github.com>
2021-04-07 23:21:13 +0300

    Add files via upload

--- 3/3.js
+++ 3/3.js
@@ -2,11 +2,16 @@
 
 let arr
 
-arr = 'arr: 2 3 5 7 '
-for (let i = 5; i < 101; i++) {
-    if (i % 2 === 0 || i % 3 === 0 || i % 5 === 0 || i % 7 === 0)
-        continue
-    arr += i
-    arr += ' '
+arr = ''
+for (let i = 2, j; i < 101; i++) {
+    for (j = 2; j < i; j++) {
+        if (i % j === 0) {
+            break
+        }
+    }
+    if (j === i){
+        arr += i
+        arr += ' '
+    }
  }
 alert(arr)
